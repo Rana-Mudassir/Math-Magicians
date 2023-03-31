9d32c054cccf1bf33830812c0dfbb3c41c85949a
Rana-Mudassir <mduasire38@gmail.com>
2023-03-31 14:47:44 +0500

    Added Child Component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,85 +1,11 @@
 import React from 'react';
+import Button from './button';
 
 function Calculator() {
   return (
     <>
       <div>
-        <table className="calculator">
-          <tbody>
-            <tr>
-              <td colSpan={4} classID="display">
-                <input type="text" id="displayInput" placeholder="0" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input className="silverColor" type="button" value="Ac" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="+/-" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="%" />
-              </td>
-              <td>
-                <input className="diffColor" type="button" value="÷" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input className="silverColor" type="button" value="7" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="8" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="9" />
-              </td>
-              <td>
-                <input className="diffColor" type="button" value="X" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input className="silverColor" type="button" value="4" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="5" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="6" />
-              </td>
-              <td>
-                <input className="diffColor" type="button" value="-" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input className="silverColor" type="button" value="1" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="2" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="3" />
-              </td>
-              <td>
-                <input className="diffColor" type="button" value="+" />
-              </td>
-            </tr>
-            <tr>
-              <td colSpan={2}>
-                <input className="silverColor" type="button" value="0" />
-              </td>
-              <td>
-                <input className="silverColor" type="button" value="." />
-              </td>
-              <td>
-                <input className="diffColor" type="button" value="=" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Button />
       </div>
     </>
   );

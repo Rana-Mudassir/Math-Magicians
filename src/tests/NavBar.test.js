fb8a7988024740acc7814293c79b311b7b459d42
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-15 09:04:21 +0100

    Work on testing

---
+++ src/tests/NavBar.test.js
@@ -0,0 +1,15 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from '../components/Navbar';
+describe('NavBar', () => {
+  test('navigates to the correct page when link is clicked', () => {
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>,
+    );
+    const calculatorLink = screen.getByText('Calculator');
+    fireEvent.click(calculatorLink);
+    expect(window.location.pathname).toBe('/calculator');
+  });
+});
